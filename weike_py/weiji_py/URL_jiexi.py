68bb664ab4140f37980be004660f2d71c44434d1
liyang <YangKaw@126.com/KawYang@163.com>
2020-05-09 17:39:15 +0800

    WeiKe

---
+++ weike_py/weiji_py/URL_jiexi.py
@@ -0,0 +1,46 @@
+#
+# /Users/mac/PycharmProjects 
+# -*- coding: utf-8 -*-
+# @Time    : 2017/8/23 上午11:21
+# @Author  : Liyang
+# @Site    : 
+# @File    : URL_jiexi.py
+# @Software: PyCharm
+#
+#
+#HTML解析器
+"""
+通过对URL下的HTML生成bs对象
+来查找有用的URL，对进行下一步的爬取做准备
+"""
+
+from bs4 import BeautifulSoup
+import re
+import urllib.parse
+from urllib.request import urlopen
+
+class JieXi():
+    def __init__(self):
+        self.data = []
+        self.new_url = ''
+    def URL_jiexi(self,new_url,html_cont = None):         #解析URL
+        try:
+            url = urlopen(new_url).read().decode("utf-8")
+            soup =BeautifulSoup (
+                url,
+                "html.parser",
+                from_encoding="utf-8"
+            )
+            new_urls = soup.find_all("a",target = "_blank",href = re.compile("^/item/[A-Za-z]+$"))
+            for url in new_urls:
+                url["href"] = "https://baike.baidu.com" + url["href"]
+                #print(url["href"])
+            self.new_url = new_urls
+            return new_urls
+        except:
+            print("file")
+
+
+
+
+
---
+++ weike_py/weiji_py/__init__.py
@@ -0,0 +1,10 @@
+#
+# /Users/mac/PycharmProjects 
+# -*- coding: utf-8 -*-
+# @Time    : 2017/8/23 上午11:21
+# @Author  : Liyang
+# @Site    : 
+# @File    : __init__.py.py
+# @Software: PyCharm
+#
+#
